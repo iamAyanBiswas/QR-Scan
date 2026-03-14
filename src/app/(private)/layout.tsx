0343a0f81dd79a91e336d3ba4b0acf235018f05d
Ayan Biswas <147629539+iamAyanBiswas@users.noreply.github.com>
2026-03-14 09:30:11 +0530

    Wraped layout.tsx with TooltipProvider

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -3,6 +3,7 @@ import { AppSidebar } from "@/components/custom/app-sidebar"
 import { ThemeProvider } from "@/components/theme-provider";
 import { ProfileDropdown } from "@/components/custom/profile";
 import { ModeToggle } from "@/components/custom/toggle";
+import { TooltipProvider } from "@/components/ui/tooltip";
 
 export default function DashboardLayout({
     children,
@@ -16,23 +17,25 @@ export default function DashboardLayout({
             enableSystem
             disableTransitionOnChange
         >
-            <SidebarProvider>
-                <AppSidebar />
-                <SidebarInset>
-                    <header className="sticky top-0 z-50 w-full border-b bg-sidebar/95 backdrop-blur supports-backdrop-filter:bg-sidebar/60 flex h-16 shrink-0 items-center justify-between gap-2 px-4">
-                        <div className="flex items-center gap-2">
-                            <SidebarTrigger className="ml-1" />
-                        </div>
-                        <div className="flex items-center gap-2">
-                            <ModeToggle />
-                            <ProfileDropdown variant="header" />
-                        </div>
-                    </header>
-                    <main className="flex flex-1 flex-col p-4 md:p-6 bg-background ">
-                        {children}
-                    </main>
-                </SidebarInset>
-            </SidebarProvider>
+            <TooltipProvider>
+                <SidebarProvider>
+                    <AppSidebar />
+                    <SidebarInset>
+                        <header className="sticky top-0 z-50 w-full border-b bg-sidebar/95 backdrop-blur supports-backdrop-filter:bg-sidebar/60 flex h-16 shrink-0 items-center justify-between gap-2 px-4">
+                            <div className="flex items-center gap-2">
+                                <SidebarTrigger className="ml-1" />
+                            </div>
+                            <div className="flex items-center gap-2">
+                                <ModeToggle />
+                                <ProfileDropdown variant="header" />
+                            </div>
+                        </header>
+                        <main className="flex flex-1 flex-col p-4 md:p-6 bg-background ">
+                            {children}
+                        </main>
+                    </SidebarInset>
+                </SidebarProvider>
+            </TooltipProvider>
         </ThemeProvider>
     );
 }
